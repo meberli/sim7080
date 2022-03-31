d1de2f6225a12387e540201eb111abd20b11b48b
meberli <eberli@gmail.com>
2022-03-31 23:48:35 +0200

    linting &co

--- redis2mqtt.py
+++ redis2mqtt.py
@@ -1,70 +1,65 @@
-#!/usr/bin/python
+"""This module listens for new entries in a redis queue
+and sends them to a mqtt broker using sim7080 integrated mqtt client."""
 
-import serial
 import time
-import redis
-import subprocess
 import logging
 import argparse
 import os
 from logging.config import fileConfig
 from getmac import get_mac_address
+import redis
+import serial
 
 CONFIG_BASE_PATH = 'conf/'
 LOGGING_CONFIG = 'logging.conf'
-
-
-
-powerKey = 4
-rec_buff = ''
-Message = 'gateway.uno'
- 
+POWER_PIN = 4
 
 def power_on():
+    """Powers on the SIM7080 module."""
     try:
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(powerKey, GPIO.OUT)
+        GPIO.setup(POWER_PIN, GPIO.OUT)
         time.sleep(0.1)
-        GPIO.output(powerKey, GPIO.HIGH)
+        GPIO.output(POWER_PIN, GPIO.HIGH)
         time.sleep(1)
-        GPIO.output(powerKey, GPIO.LOW)
+        GPIO.output(POWER_PIN, GPIO.LOW)
         time.sleep(5)
-    finally:
+    except NameError:
         logger.error('GPIO not available')
         ser.flushInput()
-        return
 
-
-def power_down(powerKey):
+def power_down():
+    """Powers down the SIM7080 module."""
     try:
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(powerKey, GPIO.OUT)
-        GPIO.output(powerKey, GPIO.HIGH)
+        GPIO.setup(POWER_PIN, GPIO.OUT)
+        GPIO.output(POWER_PIN, GPIO.HIGH)
         time.sleep(2)
-        GPIO.output(powerKey, GPIO.LOW)
+        GPIO.output(POWER_PIN, GPIO.LOW)
         time.sleep(5)
-    finally:
+    except NameError:
         logger.exception('GPIO not available')
-        return
 
 def is_running():
+    """Check if SIM7080 module is running."""
     # simcom module uart may be fool,so it is better to send much times when it starts.
-    for seq in range(3):
+    for _ in range(3):
         ser.write('AT\r\n'.encode())
         time.sleep(1)
         if ser.inWaiting():
             time.sleep(0.01)
-            recBuff = ser.read(ser.inWaiting())
-            # logger.debug(f'rec_buff: {recBuff.decode()}')
-            if 'OK' in recBuff.decode():
-                recBuff = ''
+            rec_buff = ser.read(ser.inWaiting())
+            # logger.debug(f'rec_buff: {rec_buff.decode()}')
+            if 'OK' in rec_buff.decode():
+                rec_buff = ''
                 return True
-        
+
     return False
 
 def send_at(command, back, timeout):
+    """Sends AT Command to SIM7080 module."""
     rec_buff = ''
     ser.write((command+'\r\n').encode())
     time.sleep(timeout)
@@ -73,7 +68,9 @@ def send_at(command, back, timeout):
         rec_buff = ser.read(ser.inWaiting())
     if rec_buff != '':
         if back not in rec_buff.decode():
-            logger.warning(f'command \'{command}\' failed: expected result is \'{back}\' but got \'{rec_buff.decode()}\'')
+            logger.warning(
+                f'command \'{command}\' failed.' +
+                f'expected result is \'{back}\' but got \'{rec_buff.decode()}\'')
             return False
         else:
             logger.debug(f'command \'{command}\' succeeded. returned: \'{rec_buff.decode()}\'')
@@ -83,6 +80,8 @@ def send_at(command, back, timeout):
         return False
 
 def connect_mqtt():
+    """Connects to MQTT Server with the SIM7080 module."""
+
     logger.info('*'*8 +' connecting mqtt' + '*'*8)
     try:
         logger.info('mqtt conf')
@@ -106,18 +105,18 @@ def connect_mqtt():
                 logger.info(f'sending event: \'{elem}\'')
                 send_at(f'AT+SMPUB=\"waveshare_sub\",{len(elem)},1,0', 'OK', 1)
                 send_at(elem,'OK', 4)
-            time.sleep(10)
-        
+            time.sleep(10)       
     except:
         logger.exception('Exception while trying to connect')
         if ser != None:
             send_at('AT+SMDISC', 'OK', 1)
             send_at('AT+CNACT=0,0', 'OK', 1)
             ser.close()
-        power_down(powerKey)
+        power_down()
 
 
 def write_file(filename):
+    """Writes a file to SIM7080 module."""
     logger.info('*'*8 +' writing file' + '*'*8)
     logger.info(f'writing file {filename}')
     with open(filename) as f:
@@ -133,6 +132,7 @@ def write_file(filename):
 
 
 def connect_network():
+    """Connecrts SIM7080 module to Network."""
     logger.info('*'*8 +' connecting to network' + '*'*8)
     logger.info('Preferred Selection between CAT-M and NB-IoT')
     send_at('AT+CMNB=1', 'OK', 0.5)
@@ -143,9 +143,10 @@ def connect_network():
     logger.info('APP Network Active')
     send_at('AT+CNCFG?', 'OK', 1)
     send_at('AT+CNACT=0,1', 'OK', 1)
-  
-    
+
+
 def is_network_connected():
+    """Check if SIM7080 module is connected to network."""
     res_dict = {}
     command = "AT+CPSI"
     response_fields = ("System Mode,"
@@ -162,7 +163,7 @@ def is_network_connected():
                        "RSRP,"
                        "RSSI,"
                        "RSSNR")
-    
+
     logger.info('*'*8 + ' network check' + '*'*8)
     logger.info('Inquiring UE system information')
     res = send_at(command + "?", 'OK', 1).splitlines()
@@ -174,14 +175,15 @@ def is_network_connected():
     res = send_at('AT+CNACT?', 'OK', 1).splitlines()
     for line in res:
         if line.startswith('+CNACT:'):
-            ip = line[13:-1]
-            if not ip.startswith("0.0.0.0"):
-                res_dict['ip'] = ip
+            ip_1 = line[13:-1]
+            if not ip_1.startswith("0.0.0.0"):
+                res_dict['ip'] = ip_1
                 return res_dict
     return False
-        
-    
+
+
 def info():
+    """Prints info from SIM7080 module."""
     logger.info('*'*8 + ' info ' + '*'*8)
     # logger.info('Report mobile equipment error') # enable debug messages
     send_at('AT+CMEE=2', 'OK', 1)
@@ -195,7 +197,6 @@ def info():
     send_at('AT+CSIMLOCK?', 'OK', 0.5)
     logger.info('Enter PIN')
     send_at('AT+CPIN?', 'OK', 0.5)
-    
     send_at('AT+CNACT?', 'OK', 1)
     logger.info('Inquiring UE system information')
     send_at('AT+CPSI?', 'OK', 1)
@@ -205,22 +206,28 @@ if __name__ == '__main__':
     #parse arguments
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='commands', dest='command')
-    
     # create the parser for the "write_file" command
     parser_test= subparsers.add_parser('write_file', help="write file to sim7080 module")
     parser_test.add_argument("filename", help="name of the file to send to sim7080", type=str)
 
     parser_test= subparsers.add_parser('connect', help="send downlink to device")
-    
     parser.add_argument("--port", help="serial port for modem", type=str,required=True)
-    parser.add_argument("--config_dir", default = CONFIG_BASE_PATH, help="relative path to configuration files.", type=str)
+    parser.add_argument(
+        "--config_dir",
+        default = CONFIG_BASE_PATH,
+        help="relative path to configuration files.",
+        type=str
+    )
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
                     action="store_true")
     parser.add_argument("-t", "--test", help="don't send anything to mqtt",
-                    action="store_true")    
-      
-    args = parser.parse_args()    
-    path_log_config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), args.config_dir, LOGGING_CONFIG)
+                    action="store_true")
+
+    args = parser.parse_args()
+    path_log_config_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        args.config_dir,
+        LOGGING_CONFIG)
 
     print('log config file: ',path_log_config_file)
 
@@ -234,10 +241,10 @@ if __name__ == '__main__':
         logger.info("using logging conf from {}".format(path_log_config_file))
     if args.test:
             logger.info("Test mode - not sending anything...")
-    
+
     # initialize redis
     r = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
-    
+
     # initialize serial
     ser = serial.Serial(args.port, 9600)
     ser.flushInput()
@@ -247,7 +254,7 @@ if __name__ == '__main__':
         import RPi.GPIO as GPIO
     except:     
         logger.warning('Unable to import Rpi.GPIO')
-    
+
     mac_addr = get_mac_address()
     logger.info(f'mac addr is :\'{mac_addr}\'')
 
@@ -256,7 +263,7 @@ if __name__ == '__main__':
     else:
         logger.info('sim7080 is powered off. power on..')
         power_on()
-    
+
     ip = is_network_connected()
     if ip:
         logger.info(f'already connected to network. ip is {ip}')
@@ -268,7 +275,4 @@ if __name__ == '__main__':
 
     elif args.command == 'connect':
         logger.info('Starting mqtt client')
-        connect_mqtt() 
-        
-        
-        
+        connect_mqtt()
